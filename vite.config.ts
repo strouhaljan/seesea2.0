ea7cef20e9509843bb31ceaac9b19960c45f67ba
Jan Strouhal <jan.strouhal@driven.world>
2025-04-01 20:47:40 +0200

    fix: vite config server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       "/api": {
         target: "https://app.seesea.cz",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
       },
     },
   },
