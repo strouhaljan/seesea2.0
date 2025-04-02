e782668d9e6e4503ff5411a55326461d6ae70fca
Jan Strouhal <jan.strouhal@driven.world>
2025-04-02 07:05:06 +0200

    updated navigation

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import "./App.css";
 function App() {
   return (
     <div className="app-container">
-      <header>
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "row",
+        }}
+      >
         <h1>SeeSea 2.0</h1>
         <nav className="main-nav">
           <Link
